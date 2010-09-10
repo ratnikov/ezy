cb6cc5bc1dc7c2afbadd8449fd17729d2aba163e
Dmitry Ratnikov <ratnikov@gmail.com>
2010-09-10 00:15:44 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezy::Application.config.secret_token = '1dff265fa17363163869ed94a10085b22d8a3c51c906b2c1132950f768bfadf726c93aac8824f31d05cdabd34da51a7e894464835eb06f1e64bc49ac993a7bce'
